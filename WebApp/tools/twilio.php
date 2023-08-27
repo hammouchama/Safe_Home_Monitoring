f078c1081207c714c1014df967382ea74486329f
MarwanEA <Marwanelaoumrani@gmail.com>
2023-08-27 23:27:58 +0100

    Update twilio.php

--- WebApp/tools/twilio.php
+++ WebApp/tools/twilio.php
@@ -5,13 +5,13 @@ use Twilio\Rest\Client;
 function sendMessage($msg,$tel){
     // Your Account SID and Auth Token from twilio.com/console
     // To set up environmental variables, see http://twil.io/secure
-    $account_sid = 'AC0fd2b4bb2c37713179b76a51fa69ffb1';
-    $auth_token = 'fa7bc8ad5c1155d1171fb4f2529d05e2';
+    $account_sid = '################';
+    $auth_token = '################';
     // In production, these should be environment variables. E.g.:
     // $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
 
     // A Twilio number you own with SMS capabilities
-    $twilio_number = "+12542847539";
+    $twilio_number = "################";
 
     $client = new Client($account_sid, $auth_token);
     $message = $client->messages->create(
